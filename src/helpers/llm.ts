cff9b7c5106d22b2e0e3f6117d289874a6a3246e
Tomas Dvorak <toomas2d@gmail.com>
2024-10-17 19:56:23 +0200

    feat: change LLM_PROVIDER to LLM_BACKEND
    Signed-off-by: Tomas Dvorak <toomas2d@gmail.com>

--- src/helpers/llm.ts
+++ src/helpers/llm.ts
@@ -10,60 +10,64 @@ import { GroqChatLLM } from "bee-agent-framework/adapters/groq/chat";
 import { Ollama } from "ollama";
 import Groq from "groq-sdk";
 
-export enum LLMProviders {
-  BAM = "bam",
-  WATSONX = "watsonx",
-  OLLAMA = "ollama",
-  OPENAI = "openai",
-  GROQ = "groq",
-}
+export const Providers = {
+  BAM: "bam",
+  WATSONX: "watsonx",
+  OLLAMA: "ollama",
+  OPENAI: "openai",
+  GROQ: "groq",
+} as const;
+type Provider = (typeof Providers)[keyof typeof Providers];
 
-export function getChatLLM(provider?: LLMProviders): ChatLLM<ChatLLMOutput> {
-  provider = provider || parseEnv("LLM_PROVIDER", z.nativeEnum(LLMProviders), LLMProviders.OLLAMA);
+export const LLMFactories: Record<Provider, () => ChatLLM<ChatLLMOutput>> = {
+  [Providers.BAM]: () =>
+    BAMChatLLM.fromPreset(getEnv("GENAI_MODEL") || "meta-llama/llama-3-1-70b-instruct", {
+      client: new BAMSDK({
+        apiKey: getEnv("GENAI_API_KEY"),
+      }),
+    }),
+  [Providers.GROQ]: () =>
+    new GroqChatLLM({
+      modelId: getEnv("GROQ_MODEL") || "llama-3.1-70b-versatile",
+      client: new Groq({
+        apiKey: getEnv("GROQ_API_KEY"),
+      }),
+    }),
+  [Providers.OPENAI]: () =>
+    new OpenAIChatLLM({
+      modelId: getEnv("OPENAI_MODEL") || "gpt-4o",
+      parameters: {
+        temperature: 0,
+        max_tokens: 2048,
+      },
+    }),
+  [Providers.OLLAMA]: () =>
+    new OllamaChatLLM({
+      modelId: getEnv("OLLAMA_MODEL") || "llama3.1:8b",
+      parameters: {
+        temperature: 0,
+        repeat_penalty: 1,
+        num_predict: 2000,
+      },
+      client: new Ollama({
+        host: getEnv("OLLAMA_HOST"),
+      }),
+    }),
+  [Providers.WATSONX]: () =>
+    WatsonXChatLLM.fromPreset(getEnv("WATSONX_MODEL") || "meta-llama/llama-3-1-70b-instruct", {
+      apiKey: getEnv("WATSONX_API_KEY"),
+      projectId: getEnv("WATSONX_PROJECT_ID"),
+    }),
+};
 
-  switch (provider) {
-    case LLMProviders.WATSONX:
-      return WatsonXChatLLM.fromPreset(
-        getEnv("WATSONX_MODEL") || "meta-llama/llama-3-1-70b-instruct",
-        {
-          apiKey: getEnv("WATSONX_API_KEY"),
-          projectId: getEnv("WATSONX_PROJECT_ID"),
-        },
-      );
-    case LLMProviders.BAM:
-      return BAMChatLLM.fromPreset(getEnv("GENAI_MODEL") || "meta-llama/llama-3-1-70b-instruct", {
-        client: new BAMSDK({
-          apiKey: getEnv("GENAI_API_KEY"),
-        }),
-      });
-    case LLMProviders.OPENAI:
-      return new OpenAIChatLLM({
-        modelId: getEnv("OPENAI_MODEL") || "gpt-4o",
-        parameters: {
-          temperature: 0,
-          max_tokens: 2048,
-        },
-      });
-    case LLMProviders.OLLAMA:
-      return new OllamaChatLLM({
-        modelId: getEnv("OLLAMA_MODEL") || "llama3.1:8b",
-        parameters: {
-          temperature: 0,
-          repeat_penalty: 1,
-          num_predict: 2000,
-        },
-        client: new Ollama({
-          host: getEnv("OLLAMA_HOST"),
-        }),
-      });
-    case LLMProviders.GROQ:
-      return new GroqChatLLM({
-        modelId: getEnv("GROQ_MODEL") || "llama-3.1-70b-versatile",
-        client: new Groq({
-          apiKey: getEnv("GROQ_API_KEY"),
-        }),
-      });
-    default:
-      throw new Error("No LLM provider has been defined (check out .env.example)!");
+export function getChatLLM(provider?: Provider): ChatLLM<ChatLLMOutput> {
+  if (!provider) {
+    provider = parseEnv("LLM_BACKEND", z.nativeEnum(Providers), Providers.OLLAMA);
   }
+
+  const factory = LLMFactories[provider];
+  if (!factory) {
+    throw new Error(`Provider "${provider}" not found.`);
+  }
+  return factory();
 }
