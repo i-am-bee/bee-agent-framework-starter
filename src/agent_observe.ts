6974033440671a893fa72bdaedd8f1c78fe21dbd
Tomas Dvorak <toomas2d@gmail.com>
2024-11-07 18:16:32 +0100

    feat: use wikipedia instead of ddg in observe
    Signed-off-by: Tomas Dvorak <toomas2d@gmail.com>

--- src/agent_observe.ts
+++ src/agent_observe.ts
@@ -2,19 +2,19 @@ import "dotenv/config.js";
 import { BeeAgent } from "bee-agent-framework/agents/bee/agent";
 import { FrameworkError } from "bee-agent-framework/errors";
 import { TokenMemory } from "bee-agent-framework/memory/tokenMemory";
-import { DuckDuckGoSearchTool } from "bee-agent-framework/tools/search/duckDuckGoSearch";
 import { OpenMeteoTool } from "bee-agent-framework/tools/weather/openMeteo";
 import * as process from "node:process";
 import { createObserveConnector, ObserveError } from "bee-observe-connector";
 import { getChatLLM } from "./helpers/llm.js";
 import { getPrompt } from "./helpers/prompt.js";
+import { WikipediaTool } from "bee-agent-framework/tools/search/wikipedia";
 
 const llm = getChatLLM();
 
 const agent = new BeeAgent({
   llm,
   memory: new TokenMemory({ llm }),
-  tools: [new DuckDuckGoSearchTool(), new OpenMeteoTool()],
+  tools: [new WikipediaTool(), new OpenMeteoTool()],
 });
 
 try {
